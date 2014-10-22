9e0f19573d965bfe8d9d139ffce7de4d973593e4
mursidyazar <mursidyazar@gmail.com>
2014-10-22 14:27:19 +0300

    commit1

--- gen/org/kod5/android/R.java
+++ gen/org/kod5/android/R.java
@@ -25,14 +25,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int textView1=0x7f070001;
-        public static final int textView2=0x7f070002;
-        public static final int webView1=0x7f070000;
+        public static final int checkBox1=0x7f070001;
+        public static final int checkBox2=0x7f070002;
+        public static final int checkBox3=0x7f070003;
+        public static final int checkBox4=0x7f070004;
+        public static final int checkBox5=0x7f070005;
+        public static final int checkBox6=0x7f070006;
+        public static final int sendButton=0x7f070007;
+        public static final int textView1=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment_main=0x7f030001;
-        public static final int kod5=0x7f030002;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
--- src/org/kod5/android/MainActivity.java
+++ src/org/kod5/android/MainActivity.java
@@ -1,54 +1,28 @@
 package org.kod5.android;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Toast;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class MainActivity extends Activity {
 
-	private WebView webView;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) { // Bu metod uygulama
-															// aç›ld›€›nda
-															// çal›ﬂt›r›lan
-															// metod.
+	protected void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		// webView'i tasar›mdakiyle ba€l›yoruz.
-		webView = (WebView) findViewById(R.id.webView1);
-
-		// webView'i JavaScript kodlar›n› çal›ﬂt›racak ﬂekilde set ediyoruz.
-		webView.getSettings().setJavaScriptEnabled(true);
-
-		// ‹ﬂlem yap›ld›€›n›n anlaﬂ›lmas› için ProgressDialog aç›yoruz.
-		final ProgressDialog progressDialog = ProgressDialog.show(this, "KOD5",
-				"Sayfa Yükleniyor...", true);
 		
-		webView.setWebViewClient(new WebViewClient() {
-
-			// Sayfa Yüklenirken bir hata oluﬂursa kullan›c›y› uyar›yoruz.
-			public void onReceivedError(WebView view, int errorCode,
-					String description, String failingUrl) {
-				Toast.makeText(getApplicationContext(), "Sayfa Yüklenemedi!",
-						Toast.LENGTH_SHORT).show();
-			}
-
-			// Sayfan›n yüklenme iﬂlemi bitti€inde progressDialog'u kapat›yoruz.
+		CheckBox cb = (CheckBox)findViewById(R.id.checkBox1);
+		cb.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
 			@Override
-			public void onPageFinished(WebView view, String url) {
-				super.onPageFinished(view, url);
-				if (progressDialog.isShowing())
-					progressDialog.dismiss();
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				
+				
 			}
 		});
-
-		//Web sayfam›z›n url'ini webView'e yüklüyoruz.
-		webView.loadUrl("http://www.kod5.org");
-
 	}
 }
